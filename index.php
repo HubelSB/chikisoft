b937582d25607580e32c2be3e757fcd4dabf572d
Fabrizzio-Esquivel-UNAS <fabrizzio.esquivel@unas.edu.pe>
2023-04-04 16:59:06 -0500

    Primera clase

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
-<?php 
+<?php
+    include 'controllers/inicial.php';
+    use controllers\inicial;
+    date_default_timezone_set('America/Lima');
+
+    $pag_principal = new controllerinicial();
+    $pag_principal->cargarportal();
+?>
+<!-- <?php 
     class principal{
-        public $dato;
+        public $dato = "hola";
         public function saluda(){
-            $dato = 'hola';
-            echo $dato;
+            echo $this->dato;
+            echo " mundo!";
         }
     }
-?>
+    $mi_instancia = new principal();
+    $mi_instancia->saluda();    
+?> -->
