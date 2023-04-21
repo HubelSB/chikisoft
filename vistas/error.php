4745835af12c538f94c4c0d66107ede9388b776e
JunniorVentura <venturajunnior@gmail.com>
2023-04-20 23:49:44 -0500

    ChikiPeliculas

---
+++ vistas/error.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title class="tile-login" >Error de usuario</title>
+    <link rel="stylesheet" href="http://localhost/chikisoft/css/estilo.css">
+</head>
+<body class="error-page">
+   <div class="error-usuario">
+        <h3>Usuario incorrecto...</h3>
+        <BR>
+        <a class="ref" href='http://localhost/chikisoft/vistas/login.php'>Regresar</a>
+   </div> 
+</body>
+</html>
